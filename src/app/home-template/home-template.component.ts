b14cb0272fc200788650a86549e1d90085aa5294
Mitul Shah <mithshah@amazon.com>
2021-02-04 12:35:43 -0500

    Added commented code for scroll to top buttons, updated side nav links

--- src/app/home-template/home-template.component.ts
+++ src/app/home-template/home-template.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
@@ -9,15 +9,44 @@ import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
   styleUrls: ['./home-template.component.scss']
 })
 export class HomeTemplateComponent implements OnInit {
+  // Side Nav
   public isMenuOpen: boolean = false;
 
   public onSidenavClick(): void {
     this.isMenuOpen = false;
   }
   
+  // Icons
   faTwitter = faTwitter
   faInstagram = faInstagram
   faLinkedinIn = faLinkedinIn
+
+  // Scroll to Top Button
+  // isShow: boolean | undefined;
+  // topPosToStartShowing = 100;
+
+  // @HostListener('window:scroll', ['$event'])
+  // checkScroll() {
+  //   const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+  //   console.log('[scroll]', scrollPosition);
+    
+  //   if (scrollPosition >= this.topPosToStartShowing) {
+  //     this.isShow = true;
+  //   } else {
+  //     this.isShow = false;
+  //   }
+  // }
+
+  // gotoTop() {
+  //   console.log('Inside go to top function')
+  //   window.scroll({ 
+  //     top: 0, 
+  //     left: 0, 
+  //     behavior: 'smooth' 
+  //   });
+  // }
+
   constructor() { }
 
   ngOnInit(): void {
